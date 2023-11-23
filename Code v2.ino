f6ad9874b6d154078be850457c9d1ab93dc05d5b
Edwin Telle <Edwin.telle@hotmail.com>
2023-11-23 04:27:02 +0100

    restructure files

--- Code v2.ino
+++ Code v2.ino
@@ -9,10 +9,9 @@
 #define CLK1 19 // display 1
 
 
-
- 
 TM1637Display display0(CLK0, DIO0); // red display
 TM1637Display display1(CLK1, DIO1); // blue display
+
 
 Pushbutton pushButtonRed(A0); // red button
 Pushbutton pushButtonBlue(A1); // blue button
@@ -28,33 +27,55 @@ void lightUpdate(String color){
   digitalWrite(4, LOW);
   digitalWrite(5, HIGH);
   digitalWrite(6, HIGH);
+
+  //internal led
+  digitalWrite(10, LOW); // BLUE
+  digitalWrite(11, HIGH); // RED
   }
+
   if (color == "Blue"){
   digitalWrite(4, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(6, LOW);
+
+  //internal led
+  digitalWrite(10, HIGH); // BLUE
+  digitalWrite(11, LOW); // RED
+
   }
+  
   if (color == "Natrual"){
   digitalWrite(4, HIGH);
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
+  
+  //internal led
+  digitalWrite(10, LOW); // BLUE
+  digitalWrite(11, LOW); // RED
   }
+
 }
 
-void gameUpdate(){
-  bool redStatus = pushButtonRed.isPressed();
-  bool blueStatus = pushButtonBlue.isPressed();
+bool redStatus = pushButtonRed.isPressed();
+bool blueStatus = pushButtonBlue.isPressed();
 
-  if(redStatus & blueStatus){
+void gameUpdate(){
+  redStatus = pushButtonRed.isPressed();
+  blueStatus = pushButtonBlue.isPressed();
+
+  if(!redStatus & !blueStatus){
     pointOwner = "Natrual";
+    lightUpdate(pointOwner);
+
   }
-  if(redStatus){
+  else if(!redStatus){
     pointOwner = "Red";
     lightUpdate(pointOwner);
 
   }
-  if(blueStatus){
+  else if(!blueStatus){
     pointOwner = "Blue";
+    lightUpdate(pointOwner);
   }
 
 }
@@ -67,14 +88,21 @@ void scoreUpdate(){
     scoreBlueTeam++;
   }
 
+  display0.showNumberDec(scoreRedTeam);
+  display1.showNumberDec(scoreBlueTeam);
+
+
+
 }
 
 
+
 void setup() {
+  Serial.begin(9600);
 
   // input buttons
-  pinMode(A0, INPUT); // red
-  pinMode(A1, INPUT); // blue
+  pinMode(A1, INPUT); // red
+  pinMode(A0, INPUT); // blue
   
   // outgoing led strip pins
   pinMode(4, OUTPUT);  // red
@@ -101,17 +129,22 @@ void setup() {
   display1.showNumberDec(0, true); // sets display to all 0
 
   // led strip bootup
-
   digitalWrite(4, HIGH); // set red off
   digitalWrite(5, LOW); // set green on
   digitalWrite(6, HIGH); // set blue off
   digitalWrite(7, HIGH); // set white off
+
+  //digitalWrite(8, HIGH);
+  //digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH); // BLUE
+
+  //digitalWrite(13, HIGH);
+  digitalWrite(11, HIGH); // RED
+  //digitalWrite(12, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   gameUpdate();
+  scoreUpdate();
   delay(tick);
-
-
 }
